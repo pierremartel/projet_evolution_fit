92167235ca0caf1315d1659c5b1f7c00087536b5
pierre.martel <pmartel502@gmail.com>
2023-01-30 12:39:14 +0100

    fix problem show picture category

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -20,7 +21,19 @@ class CategoryType extends AbstractType
             ])
             ->add('picture', FileType::class, [
                 'label' => 'Photo de la catégorie',
-                'attr' => ['placeholder' => 'Photo de la catégorie']
+                'attr' => ['placeholder' => 'Photo de la catégorie'],
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Le format de l\'image importer n\'est pas correct',
+                    ])
+                ],
             ])
 
             ;
